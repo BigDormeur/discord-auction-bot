fbab5c8cb8e42e5053c5188cda3763f71b18d8a2
Timor <yanismanca66@gmail.com>
2026-06-11 11:14:24 +0200

    Delete events directory

--- events/ready.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'ready',
-  once: true,
-  execute(client) {
-    console.log(`🤖 ${client.user.tag} est prêt !`);
-    client.user.setActivity('les enchères 🔨', { type: 3 }); // WATCHING
-  },
-};
